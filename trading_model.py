e5696fadae671ced891c9a8b7f836ac50c21e267
ArtjomsCubacins <166109704+23DP2ACuba@users.noreply.github.com>
2025-06-07 20:51:26 +0300

    Create trading_model.py

---
+++ trading_model.py
@@ -0,0 +1,80 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+import pandas_ta as ta
+import yfinance as yf
+import pandas as pd
+
+# ---------- config ----------
+lookahead = 2
+target_pct = 2
+lookback = 2
+symbol = "BTC-USD"
+period = "1d"
+start="2020-01-01"
+end="2025-06-01"
+# ----------------------------
+data = yf.Ticker(symbol).history(period=period, start=start, end=end)
+def get_features(data):
+  data["HLpct"] = ((data.High - data.Low) / data.High) * 100
+  data["PCT_Change"] = ((data.Close - data.Open) / data.Open) * 100
+
+  data["SMA_100"] = ta.ema(data.Close, length=100)
+  data["SMA_200"] = ta.ema(data.Close, length=200)
+
+  data["ADX"] = ta.adx(high=data["High"], low=data["Low"], close=data["Close"])["ADX_14"]
+
+  data["ATR"] = ta.atr(high=data["High"], low=data["Low"], close=data["Close"])
+
+  data["VWAP"] = ta.vwap(high=data["High"], low=data["Low"], close=data["Close"], volume=data["Volume"])
+  global features
+  features = ["HLpct", "PCT_Change", "SMA_100", "SMA_200", "ADX", "ATR", "VWAP"]
+
+  for feature in features:
+      data[f"{feature}_rolling_{lookback}"] = data[feature].rolling(window=lookback).mean()
+  return data
+
+def get_target(data):
+  target = pd.DataFrame(data["Close"])
+
+  for i in range(1, lookahead+1):
+    target[f"Close{i}"] = target["Close"].shift(-i)
+
+  pct_changes = [(target[f"Close{i}"] / target["Close"] - 1) * 100 for i in range(1, lookahead + 1)]
+  data["Target"] = pd.concat(pct_changes, axis=1).ge(target_pct).any(axis=1).astype(int)
+  data.dropna(inplace=True)
+  return data
+data = get_features(data)
+data = get_target(data)
+x = data[data.columns.difference(["Target", "Close"])]
+y = data["Target"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, shuffle=False)
+
+pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", SVC(max_iter=10000))
+])
+
+pipeline.fit(x_train, y_train)
+y_pred = pipeline.predict(x_test)
+print("Accuracy score: ", accuracy_score(y_test, y_pred))
+
+#Test
+l = []
+for i in range(200, 1, -1):
+  test_data = x_test.tail(i)
+  test_target = y_test.tail(i)
+  test_data = test_data.head(1)
+  test_target = test_target.head(1)
+  y_pred = pipeline.predict(test_data)
+  l.append(y_pred.item() == test_target.item())
+  print(f"pred: {y_pred}, y_act: {test_target.item()}, {y_pred.item() == test_target.item()}")
+
+import collections
+collections.Counter(l)
