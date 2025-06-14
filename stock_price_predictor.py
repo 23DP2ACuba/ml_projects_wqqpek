c946ef589313dd1d32baaf924baef3b67a3a6fdc
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2025-06-14 08:42:01 +0300

    Update stock_price_predictor.py

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,10 +1,10 @@
-"""
-Stock pricepredictor
-"""
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor 
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
-import xgboost as xgm
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score
+from xgboost import XGBRegressor
 import yfinance as yf
 
 
@@ -16,6 +16,27 @@ df["Return"] = (df.Close - df.Open) / df.Open
 df["Volatility"] = df.Return.rolling(window=14).std()
 df["HL_pct"] = ((df.High - df.Low) / df.High) * 100
 df["TP"] = ((df.High + df.Low + df.Close) / 3) * df.Volume
-df["VWAP"] = df.TP.cumsum() / df.Volume.cumsum()
-df[["VWAP", "HL_pct", "Volatility", "Return", "Close", "Volume"]]
-df.tail(5)
+df["VWAP"] = df["TP"].rolling(window=14).sum() / df["Volume"].rolling(window=14).sum()
+df = df[["VWAP", "HL_pct", "Volatility", "Return", "Close", "Volume"]]
+
+df["Target"] = df["Close"].shift(-1)
+df.dropna(inplace=True)
+
+x = df[df.columns.difference(["Target"])]
+y = df["Target"]
+x_train, x_test, y_train,  y_test = train_test_split(x, y, test_size=.20)
+
+pipeline = Pipeline([
+    ('pca', PCA(n_components=5)),
+    ('scaler', StandardScaler()),
+    ('xgb', XGBRegressor(
+        n_estimators=10000,
+        max_depth=5,
+        learning_rate=0.01
+    ))
+])
+
+pipeline.fit(x_train, y_train)
+y_pred = pipeline.predict(x_test)
+accuracy = r2_score(y_test, y_pred)
+print(f"model r2_score: {accuracy}")
