162678ce20f6b33b84f58dbf5020a87057a5b644
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2025-06-16 18:52:05 +0300

    Update stock_price_predictor.py

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,3 +1,7 @@
+"""
+Stock pricepredictor
+"""
+#%%
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
@@ -8,7 +12,7 @@ from xgboost import XGBRegressor
 import yfinance as yf
 
 
-df = yf.Ticker("ETH-USD").history(period = "1d", start = "2021-06-01", end = "2025-06-01")
+df = yf.Ticker("ETH-USD").history(period = "1d", start = "2021-06-01", end = "2025-05-15")
 df = df[["Open", "High",	"Low",	"Close", 	"Volume"]]
 
 
@@ -24,7 +28,8 @@ df.dropna(inplace=True)
 
 x = df[df.columns.difference(["Target"])]
 y = df["Target"]
-x_train, x_test, y_train,  y_test = train_test_split(x, y, test_size=.20)
+#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20)
+#x_test, x_val, y_test, y_val = train_test_split(x, y, test_size=.25)
 
 pipeline = Pipeline([
     ('pca', PCA(n_components=5)),
@@ -32,11 +37,28 @@ pipeline = Pipeline([
     ('xgb', XGBRegressor(
         n_estimators=10000,
         max_depth=5,
-        learning_rate=0.01
+        learning_rate=0.01,
+        verbosity = 1
     ))
 ])
 
 pipeline.fit(x_train, y_train)
-y_pred = pipeline.predict(x_test)
-accuracy = r2_score(y_test, y_pred)
-print(f"model r2_score: {accuracy}")
+
+#%%
+df = yf.Ticker("ETH-USD").history(period = "1d", start = "2025-04-1", end = "2025-06-15")
+df = df[["Open", "High",	"Low",	"Close", 	"Volume"]]
+
+end = "2025-05-12"
+target = "2025-05-7"
+
+df["Return"] = (df.Close - df.Open) / df.Open
+df["Volatility"] = df.Return.rolling(window=14).std()
+df["HL_pct"] = ((df.High - df.Low) / df.High) * 100
+df["TP"] = ((df.High + df.Low + df.Close) / 3) * df.Volume
+df["VWAP"] = df["TP"].rolling(window=14).sum() / df["Volume"].rolling(window=14).sum()
+df = df[['Close', 'HL_pct', 'Return', 'VWAP', 'Volatility', 'Volume']]
+
+
+row = df.loc[[end]]
+y_pred = pipeline.predict(row)
+print(*y_pred)
