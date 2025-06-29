33d7d50b41c4d163d3c0b6501ea73c8318f9bc2c
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2025-06-29 11:42:50 +0300

    added modified gpt model gpt_model.py

--- trade_gpt/gpt_model.py
+++ trade_gpt/gpt_model.py
@@ -4,7 +4,7 @@ GPT-model.py
 
 import torch
 import torch.nn as nn
-from torch.nn import functional as f
+from torch.nn import functional as F
 from torch.utils.data import DataLoader, Dataset
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -32,7 +32,10 @@ LEARNING_RATE = 0.001
 n_embd = 384
 block_size = 8
 max_iters = 1000
-eval_iter = 100
+eval_iter = 10
+eval_interval = 50
+n_layer = 6
+n_head = 6
 DROPOUT = 0.3
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # ----------------------------------
@@ -89,7 +92,7 @@ features = train_data.columns
 
 scaler = StandardScaler()
 train_data = scaler.fit_transform(train_data)
-val_data = scaler.fit_transform(val_data)
+val_data = scaler.transform(val_data)
 
 train_data = torch.tensor(train_data, dtype=torch.float32)
 val_data = torch.tensor(val_data, dtype=torch.float32)
@@ -100,9 +103,116 @@ n_input_features = len(features)
 
 def get_batch(train=True):
     data_split = train_data if train else val_data
-    ix = torch.randint(len(data_split) - block_size, (BATCH_SIZE,))
-    x = torch.stack([data_split[i:i+block_size] for i in ix]).to(device)
-    y = torch.stack([data_split[i+1:i+block_size+1] for i in ix]).to(device)
+    max_start = len(data_split) - block_size - 1
+    ix = torch.randint(max_start, (BATCH_SIZE,))
+    x = torch.stack([data_split[i:i + block_size] for i in ix]).to(device)
+    y = torch.stack([data_split[i + 1:i + block_size + 1] for i in ix]).to(device)
     return x, y
 
-get_batch()
+def get_eval_batch(seq_start=0):
+    x = val_data[seq_start:seq_start+block_size].unsqueeze(0).to(device)
+    y = val_data[seq_start+1:seq_start+block_size+1].unsqueeze(0).to(device)
+    return x, y
+
+class Head(nn.Module):
+  def __init__(self, head_size):
+    super().__init__()
+    self.key = nn.Linear(n_embd, head_size, bias = False)
+    self.query = nn.Linear(n_embd, head_size, bias = False)
+    self.value = nn.Linear(n_embd, head_size, bias = False)
+    self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(DROPOUT)
+
+  def forward(self, x):
+    b, t, c = x.shape
+    k = self.key(x)
+    q = self.query(x)
+    wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
+    wei = wei.masked_fill(self.tril[:t, :t] == 0, float('-inf'))
+    wei = f.softmax(wei, dim=-1)
+    v = self.value(x)
+    out = wei @ v
+    return out
+
+class MultiHead(nn.Module):
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj = nn.Linear(head_size * num_heads, n_embd)
+    self.dropout = nn.Dropout(DROPOUT)
+
+  def forward(self, x):
+    out = torch.cat([h(x) for h in self.heads], dim=-1)
+    out = self.dropout(self.proj(out))
+    return out
+
+class FeedForward(nn.Module):
+  def __init__(self, n_embd):
+    super().__init__()
+    self.net = nn.Sequential(
+        nn.Linear(n_embd, 4 * n_embd),
+        nn.ReLU(),
+        nn.Linear(n_embd * 4, n_embd),
+        nn.Dropout(DROPOUT)
+    )
+
+  def forward(self, x):
+    return self.net(x)
+
+class Block(nn.Module):
+  def __init__(self, n_head, n_embd):
+    super().__init__()
+    head_size = n_embd // n_head
+    self.sa = MultiHead(n_head, head_size)
+    self.fwd = FeedForward(n_embd)
+    self.l1 = nn.LayerNorm(n_embd)
+    self.l2 = nn.LayerNorm(n_embd)
+
+  def forward(self, x):
+    x = x + self.sa(self.l1(x))
+    x = x + self.fwd(self.l2(x))
+    return x
+
+class GPT(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.input_projection = nn.Linear(n_input_features, n_embd)
+    self.position_embedding_table = nn.Embedding(block_size, n_embd)
+    self.block = nn.Sequential(*[Block(n_head, n_embd) for _ in range(n_layer)])
+    self.ln = nn.LayerNorm(n_embd)
+    self.lm_head = nn.Linear(n_embd, n_input_features) 
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+    if isinstance(module, nn.Linear):
+      nn.init.normal_(module.weight, mean=0.0, std=0.2)
+      if module.bias is not None:
+        nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+      nn.init.normal_(module.weight, mean=0.0, std=0.2)
+
+  def forward(self, idx, target=None):
+    b, t, f = idx.shape  
+    tok_emd = self.input_projection(idx)  
+    pos_emd = self.position_embedding_table(torch.arange(t, device=idx.device)).unsqueeze(0)  
+    x = tok_emd + pos_emd  
+    x = self.block(x)
+    x = self.ln(x)
+    logits = self.lm_head(x)  
+
+    if target is not None:
+        loss = F.mse_loss(logits, target)  
+    else:
+        loss = None
+
+    return logits, loss
+
+  def generate(self, idx, max_new_tokens):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -block_size:]
+        logits, _ = self(idx_cond)
+        next_step = logits[:, -1:, :]
+        idx = torch.cat((idx, next_step), dim=1)
+    return idx
+
+model = GPT().to(device)
